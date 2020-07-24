969592d64ca7858e7c9d5aa21e518dd5002a4b32
Gayane Mirzoyan <gayusha@gmail.com>
2020-07-24 14:16:44 -0700

    Updates

---
+++ precache-manifest.f073cfd0c55488213436852d108683ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804a1d03f624b7d9368d6caf30d3211b",
+    "url": "/cra6/index.html"
+  },
+  {
+    "revision": "9c0c81c751ccb252c851",
+    "url": "/cra6/static/css/main.3990d7f2.chunk.css"
+  },
+  {
+    "revision": "6b38f591fa6a8f7874dc",
+    "url": "/cra6/static/js/2.093e4767.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra6/static/js/2.093e4767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0c81c751ccb252c851",
+    "url": "/cra6/static/js/main.bbbcbe24.chunk.js"
+  },
+  {
+    "revision": "0be39bd4c23e6ef94ed2",
+    "url": "/cra6/static/js/runtime-main.92956e98.js"
+  }
+]);
